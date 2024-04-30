803488738bb17584283c8f35a43d232484bd9592
Marc Ramilo Garrido <marc.ramilo@cirvianum.cat>
2024-04-30 17:55:49 +0200

    commit 3

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,1 +1,5 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
+async function request(endpoint) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  return response.json();
+}
--- UF3/validacions.js
+++ UF3/validacions.js
@@ -50,6 +50,7 @@ document
     // Validacions dels camps del formulari.
 
     // Validar que el campname i surname tinguin un min i max de caràcters.
+
     if (name.length < 3 || !name.length > 50) {
       alert("El nom han de tenir com a mínim 3 caràcters i menys de 50");
       return;
@@ -94,3 +95,45 @@ document
       return;
     }
   });
+
+const products = [
+  {
+    category: "Electrònica",
+    price: 499.99,
+    name: "Telèfon Intel·ligent",
+  },
+  { category: "Electrònica", price: 799.99, name: "Portàtil" },
+  {
+    category: "Electrònica",
+    price: 299.99,
+    name: "Auriculars sense fils",
+  },
+  {
+    category: "Electrònica",
+    price: 199.99,
+    name: "Televisor Intel·ligent",
+  },
+  {
+    category: "Electrònica",
+    price: 149.99,
+    name: "Relotge Intel·ligent",
+  },
+  { category: "Llar", price: 249.99, name: "Rentadora" },
+  { category: "Llar", price: 149.99, name: "Assecadora" },
+  { category: "Llar", price: 99.99, name: "Cafetera" },
+  { category: "Llar", price: 79.99, name: "Torradora" },
+  { category: "Llar", price: 199.99, name: "Aspiradora" },
+];
+
+const selector = document.getElementById("product");
+
+products.forEach((product) => {
+  const producte = document.createElement("option");
+  producte.innerText = product.name;
+  selector.appendChild(producte);
+});
+
+const producteActual = document.getElementById('')
+if (producte) {
+  const quantitat = document.getElementById("quantity");
+}
